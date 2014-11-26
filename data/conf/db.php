50efde5e3d007b22040ecea516ccacf7feaa5896
linconphp <lincolnphp@126.com>
2014-11-26 12:59:18 +0800

    thinkCMF_x版本

---
+++ data/conf/db.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'thinkcmf',
+    'DB_USER' => 'root',
+    'DB_PWD' => '',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'cx_',
+    //密钥
+    "AUTHCODE" => 'XWmVkzW0HTmUqagSDz',
+    //cookies
+    "COOKIE_PREFIX" => '8HEyLR_',
+);
+?>
